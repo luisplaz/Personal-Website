713bc5e324b52873d22de1ce2a482643cd3e031a
luisplaz <luisplaz@hotmail.com>
2016-02-15 16:57:14 -0500

    Add Js file to repo

---
+++ javascript/text.js
@@ -0,0 +1,201 @@
+var skype = '<script type="text/javascript" src="http://www.skypeassets.com/i/scom/js/skype-uri.js"></script>'+
+'<div id="SkypeButton_Call_luisplaz_1">'+
+' <script type="text/javascript">'+
+' Skype.ui({'+
+' "name": "dropdown",'+
+' "element": "SkypeButton_Call_luisplaz_1",'+
+' "participants": ["luisplaz"]'+
+' });'+
+' </script>'+
+'</div>';
+	
+var home = "<div>\
+<h1 class=welcome-title>\
+Welcome to my page\
+</h1>\
+<p class=welcome-info>\
+Here you will find all you might need to know about me as well as my portfolio of\
+<a href='#' class='text-link' onclick='change(this);'>\
+ projects.\
+</a>\
+<br>\
+You may also contact me through the link on the bottom of the page.<br>Hope to hear from you soon!\
+</p>"+ skype +"</div>"
+
+var about = "<div>\
+<p>\
+I am a creative and driven developer from Venezuela, that has had experience in the fields of engineering and finance. My foundation as an industrial engineer has sharpened my analytical and problem solving skills and has molded the way I tackle new obstacles. My studies in finance have honed my understanding of the business side of things and has given me the tools to be able to successfully start a business of my own web based business in my home country destined to solve the difficulties of finding professional service providers.\
+</p>\
+<p>\
+My first work experience introduced me to web development, giving me the opportunity to become part of UX design teams and implement new and creative ways of optimizing user usability and experience in the company's website.  Later on, working as a monitoring coordinator for machinery software I was presented with the opportunity to  lead a web based project where I was able to work hand in hand with web developers and designers, ultimately leading to my interest in the tech industry.\
+</p>\
+<p>\
+Ever since, I have dedicated all of my time and energy to gain the required knowledge needed to fully understand how to bring my ideas to life through code and tackle larger and more complex projects. Through my courses at Columbia and the completion of a 19 week developer bootcamp I am certain I have achieved this goal.\
+</p>\
+<p>\
+Im driven by the need to elevate the quality of life through tech based undertakings and demonstrate the dynamism of tech in fields that have yet to adopt it by providing solutions through meaningful applications. \
+</p>\
+<p>\
+I see myself in a position where I can have the liberty of pitching ideas and giving life to my creations, where I can learn from other programmers and leave my footprint in the tech world.\
+</p>\
+</div>"
+
+var curriculum = "<div>\
+<ul>\
+<li class=curriculum-title>\
+PROFESSIONAL EXPERIENCE\
+</li>\
+	<ul>\
+	<p class=curriculum-sub-title>\
+	<img src='imgs/Logo-Mega.gif' class='icon-horizontal'>\
+	MEGA (KOMATSU DISTRIBUTOR), Guarenas, Venezuela 2013-2015<br> Komtrax Fleet Monitoring Coordinator/IT Manager\
+	</p>\
+	<li>\
+	Developed automatic fleet report system for construction and mining equipment, reducing time required for reporting data by 45%; implemented system health meter that reflected equipment functionality.\
+	</li>\
+	<li>\
+	Restructured all reports to persuade clients to use Komtrax System, resulting in improved consumer satisfaction and retention.\
+	</li>\
+	<li>\
+	Directed follow-up process to ensure all machines equipped with Komtrax were successfully completing satellite diagnostic process; offered customer and IT support for all of Komatsu’s Platforms.\
+	</li>\
+	<li>\
+	Projected future spare parts sales based on information analysis derived from the Komtrax system.\
+	</li>\
+	<li>\
+	Co-Led design and development of Mega’s own web and mobile monitoring platform with programming agency providing hands-on experience with programmers and programming languages.\
+	</li>\
+	<p class=curriculum-sub-title>\
+	<img src='imgs/Logo-Movistar.jpeg' class='icon-horizontal'>\
+	MOVISTAR VENEZUELA, Caracas, Venezuela 2012<br>Intern (Full-Time)\
+	</p>\
+	<li>\
+	Optimized billing and recharge systems for the largest mobile phone service provider in Venezuela; teamed with programmers to developed new coding for digital account platform, modifying the visual and sensorial interaction of many of its transactions in order to provide a more attractive and efficient customer experience.\
+	</li>\
+	<li>\
+	Coordinated marketing, design and web development divisions in order to provide an alternative that would alleviate difficulties in all aspects of customer interaction within the virtual platform.\
+	</li>\
+	<li>\
+	Developed transaction manuals and routine follow-ups to ensure optimal system implementation achieving significant improvement in customer service; proposed optimization of multiple transactions available in virtual platform “Mi Movistar”.\
+	</li>\
+	<p class=curriculum-sub-title>\
+	<img src='imgs/Logo-Plumrose.png' class='icon-vertical'>\
+	PLUMROSE VENEZUELA (FOOD INDUSTRY), Caracas, Venezuela 2012<br>Intern (Part-Time)\
+	</p>\
+	<li>\
+	Contributed to introduction and launch of all new frozen Plumrose products (Iceberg Project) including coding and registration processes; developed model to project distribution of all new frozen products, helping define launch goals.\
+	</li>\
+	<li>\
+	Analyzed and controlled sales and inventory of Eveba canned products and Conaprole cheese products.\
+	</li>\
+	<li>\
+	Developed transaction manuals and routine follow-ups to ensure optimal system implementation achieving significant improvement in customer service; proposed optimization of multiple transactions available in virtual platform “Mi Movistar”.\
+	</li>\
+	<p class=curriculum-sub-title>\
+	OTHER PROFESSIONAL EXPERIENCE 2014-Present\
+	</p>\
+	<li>\
+	Founder and CEO of web based company www.dconfianza.com that will connect Venezuelans with professional service providers such as home cleaning services, contractors, and other professionals, set to launch mid-2016.\
+	</li>\
+	<li>\
+	Supported Maquinarias Mega as freelance visual basic programmer; developed excel programs optimizing to a great scale the current after-sales reporting processes, and consequently achieving payroll reduction.\
+	</li>\
+	</ul>\
+<br>\
+<div>\
+	_____________________________________________________________________________________________________________\
+</div>\
+<br>\
+<li class=curriculum-title>\
+EDUCATION\
+</li>\
+	<ul>\
+	<p class=curriculum-sub-title>\
+	<img src='imgs/Logo-Columbia.png' class='icon-horizontal'>\
+	COLUMBIA UNIVERSITY, Graduate Foundation’s Certificate 2015, New York, NY\
+	</p>\
+	<li>\
+	Completed rigorous three-month session focused on computer science courses, learning basic Java programming, Data Structures, Statistics with calculus and social media networking for organizations.\
+	</li>\
+	<p class=curriculum-sub-title>\
+	<img src='imgs/Logo-Iesa.jpg' class='icon-vertical'>\
+	IESA, Masters in Finance 2013-2015, Caracas, Venezuela\
+	</p>\
+	<li>\
+	Completed this top 6th finance graduate program in Latin America, as ranked by Eduniversal Best Masters.\
+	</li>\
+	<p class=curriculum-sub-title>\
+	<img src='imgs/Logo-Unimet.jpg' class='icon-horizontal'>\
+	UNIVERSIDAD METROPOLITANA, Bachelor of Science in Production Engineering 2007-2013, Caracas,Venezuela\
+	</p>\
+	<li>\
+	Led thesis project “Improvements in Self-Management Transactions for Mobile Phone Operator Movistar;” collaborated with coders to implement new and improved processes in the virtual platforms, enhancing productivity and user experience.\
+	</li>\
+	<p class=curriculum-sub-title>\
+	OTHER EDUCATION\
+	</p>\
+	<li>\
+	La Sorbonne, Paris, France French Language and Civilization Course Debutant Level 2006 – 2007.\
+	</li>\
+	<li>\
+	Scuola Leonardo Da Vinci, Florence, Italy Italian Language Course Intermediate Level 2006 – 2007.\
+	</li>\
+	</ul>\
+<br>\
+<div>\
+	_____________________________________________________________________________________________________________\
+</div>\
+<br>\
+<li class=curriculum-title>\
+ADDITIONAL\n\
+</li>\
+	<ul>\
+	<br>\
+	<li>\
+	Languages: Spanish (Native tongue), English (Advanced And Fluent), Italian (Basic), French (Basic)\
+	</li>\
+	<li>\
+	Advanced computer software knowledge in: Word, Excel, PowerPoint, Visual Basic,Javascript,Html, Ruby On Ralis\
+	</li>\
+	<li>\
+	Volunteered at Voluntad Popular (political party led by Leopoldo Lopez, top contender for Hugo Chavez), led the mass fund recollection project “Ceibas;” conceptualized pyramid structure 1x10 that would optimize management of funds,2011\
+	</li>\
+	<li>\
+	Enjoy programming, boxing, listening to music, caring for my two beagles.\
+	</li>\
+	</ul>\
+</ul>\
+</div>";
+
+var projects = "<div>\
+<a class='nav-options projects-info' href=http://www.dconfianza.com/>\
+www.dconfianza.com\
+</a>\
+<div class=box>\
+<iframe src=http://www.dconfianza.com/>\
+</iframe>\
+</div>";
+
+
+function change(link)
+{
+	var txt = link.text
+	switch(txt) {
+		case "Home":
+		document.getElementById('information-container').innerHTML = home;
+		break;
+		case "About":
+		document.getElementById('information-container').innerHTML = about;
+		break;
+		case "Curriculum":
+		document.getElementById('information-container').innerHTML = curriculum;
+		break;
+		case "Projects":
+		document.getElementById('information-container').innerHTML = projects;
+		break;
+		case " projects.":
+		document.getElementById('information-container').innerHTML = projects;
+		break;
+		default:
+	}
+}
